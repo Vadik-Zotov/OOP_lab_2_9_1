76aa2fd3350163891b68a50119235a5782b98378
Роман Данилевич <dwordchanel@gmail.com>
2020-05-11 02:42:19 +0300

    Add project files.

---
+++ OOP_lab_2_9_1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace OOP_lab_2_9_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            switch (n)
+            {
+                case 1:
+                    Console.WriteLine("Зима");
+                    break;
+                case 2:
+                    Console.WriteLine("Зима");
+                    break;
+                case 3:
+                    Console.WriteLine("Весна");
+                    break;
+                case 4:
+                    Console.WriteLine("Весна");
+                    break;
+                case 5:
+                    Console.WriteLine("Весна");
+                    break;
+                case 6:
+                    Console.WriteLine("Лiто");
+                    break;
+                case 7:
+                    Console.WriteLine("Лiто");
+                    break;
+                case 8:
+                    Console.WriteLine("Лiто");
+                    break;
+                case 9:
+                    Console.WriteLine("Осiнь");
+                    break;
+                case 10:
+                    Console.WriteLine("Осiнь");
+                    break;
+                case 11:
+                    Console.WriteLine("Осiнь");
+                    break;
+                case 12:
+                    Console.WriteLine("Зима");
+                    break;
+            }
+        }
+    }
+}
